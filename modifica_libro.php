41b7e97220ff8f3aef2e05fbfdef0181108b6831
svevapepe <sveva.pepe@gmail.com>
2016-09-17 16:45:08 +0200

    Update modifica_libro.php

--- modifica_libro.php
+++ modifica_libro.php
@@ -126,7 +126,7 @@ if ($count == 1){
                     <div class="form-group">
                         <label class="control-label col-sm-2">Inserisci la tua password per confermare che sia veramente tu</label>
                         <div class="col-sm-10">
-                            <input type="password" name="password" value="" class="form-control">
+                            <input type="password" name="password" value="<?php echo $info[password] ?>" class="form-control">
                         </div>
                     </div>
                     <div class="form-group">
